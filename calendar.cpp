178d35a99f0d4b2398bca7aa57ed756a1cab60fc
DaviesHub <davieschuks.a@gmail.com>
2022-10-01 14:42:19 +0200

    Add hyphens after days. Change indices staring points to 0

--- calendar.cpp
+++ calendar.cpp
@@ -9,10 +9,28 @@ int main() {
     // declare variables
     unsigned int numDays;
     unsigned int firstDay;
+    unsigned int col = 1;
 
     cout << "Enter the number of days in the month:"<< endl;
     cin >> numDays;
 
     cout << "Enter the number (0-6) representing the first day of the month. 0 for Sunday, 6 for Saturday:" << endl;
+    cin >> firstDay;
 
+    cout << "Sun   Mon   Tue   Wed   Thu   Fri   Sat" << endl;
+    cout << "---------------------------------------" << endl;
+
+    for (int i = 1; i < firstDay; i++) {
+        cout << "      ";
+        col++;
+    }
+
+    for (int i = 1; i <= numDays; i++) {
+        if (col > 7) {
+            cout << endl;
+            col = 1;
+        }
+        col++;
+        cout << setw(3) << i << "   ";
+    }
 }
