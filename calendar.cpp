7130efaa6d9073790b7eb5a6792ba956c966e6c6
DaviesHub <davieschuks.a@gmail.com>
2022-10-01 10:56:02 +0200

    Prompt variables

---
+++ calendar.cpp
@@ -0,0 +1,18 @@
+/* This program prints a months calendar
+** The program receives the number of days in the month and the first day of the week in the month as input */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // declare variables
+    unsigned int numDays;
+    unsigned int firstDay;
+
+    cout << "Enter the number of days in the month:"<< endl;
+    cin >> numDays;
+
+    cout << "Enter the number (0-6) representing the first day of the month. 0 for Sunday, 6 for Saturday:" << endl;
+
+}
